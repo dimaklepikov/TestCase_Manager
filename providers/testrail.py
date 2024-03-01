5facef7de19dfacbf83da6d9daa64e4cf89c2cd8
Dmitriy Klepikov <klepikovdima@gmail.com>
2024-03-01 15:53:14 +0200

    Update providers && Enrich with the new method

--- providers/testrail.py
+++ providers/testrail.py
@@ -4,35 +4,55 @@ from models.test_case import TestCase
 from providers.base_provider import Provider
 from lib.connection import Connection
 
+# TODO: Use Python SDK for TestRail (low priority) - if it's available
+
+class TRConfiguration:
+    """This object stores the configuration for the TestRail which is not siupposed to be changed"""
+    def __init__(self, project_id: int, suite_name: str, section: str):
+        self.project_id: int = project_id
+        self.suite_name: str = suite_name
+        self.section: str = section
+        # TODO: enrich from API
+        self.case_fields = None
+
 
 class TestRail(Provider):
-    # TODO: Add TestRail specific methods
+    # TODO: Implement persistent checks on id's of created entities by code (Sqllite) (high priority)
     # TODO: Add id/name dispatching during initiation (high priority)
+    # TODO: Add TestRail specific methods
     # TODO: Add async support (low priority)
 
-    def __init__(self, credentials: dict = TR):
+    def __init__(
+            self,
+            credentials: dict = TR,
+            configuration: TRConfiguration = TRConfiguration(
+                project_id=3, suite_name="[TestCaseManager] Mock", section="Test Section"
+            )
+    ):
         self.connection = Connection(
             url="https://pluto.testrail.io/index.php?/api/v2/", credentials=credentials
         )
-        self.project_id: int = 3
-        self.suite_name: str = "[TestCaseManager] Mock"
-        self.section: str = "Test Section"
+        self.configuration = configuration
+        # TODO: Generate by the response
+        self.case_fields = {}
+
+        self.test_case = TestCase("Test", "Test description")
 
     def _get_suite(self):
         # TODO: ADD check by ID (Add for provider class)
         # TODO: Add suit creation feature (low priority)
         for suite in self.connection.http(
-            "GET", f"get_suites/{self.project_id}"
+            "GET", f"get_suites/{self.configuration.project_id}"
         ).json():
-            if suite["name"] == self.suite_name:
+            if suite["name"] == self.configuration.suite_name:
                 return suite["id"]
         return None
 
     def _get_section(self):
         for sec in self.connection.http(
-            "GET", f"get_sections/{self.project_id}&suite_id={self._get_suite()}"
+            "GET", f"get_sections/{self.configuration.project_id}&suite_id={self._get_suite()}"
         ).json()["sections"]:
-            if sec["name"] == self.section:
+            if sec["name"] == self.configuration.section:
                 return sec["id"]
 
     def create_case(self, test_case: TestCase):
@@ -40,14 +60,49 @@ class TestRail(Provider):
         # TODO: Add the check for existing case (if the same name exists)
         # TODO: Add the object to update the case (if created)
         # TODO: Rename the method to CASE (low priority)
+
+        # Create case: "https://support.testrail.com/hc/en-us/articles/15760060756116-Creating-test-cases#creating-test-cases-with-test-case-steps-template-0-1"
+
         payload = {"title": test_case.title}
 
         return self.connection.http(
             "POST", f"add_case/{self._get_section()}", json=payload
         ).json()
 
+    # def get_case_fields(self):
+    #     return self.connection.http(
+    #         "GET", f"get_case_fields"
+    #     ).json()
+
+    def construct_custom_fields(self, fields: list = None) -> dict:
+        """
+        This method constructs the custom fields for the test case
+
+        :arg fields: list - list of fields (equal to UI TR names to be added to the test case)
+
+        TBD:
+        :return: dict - custom fields for the TR test case
+        """
+        # Define the labels (real names) list of TR custom fields
+        case_fields = self.connection.http(
+            "GET", f"get_case_fields"
+        ).json()
+
+        labels = [case["label"] for case in case_fields]
+
+        # Object to pass to test case creation
+        custom_fields_system_names = {}
+
+        for field in fields:
+            if field not in labels:
+                raise ValueError(f"Field {field} is not in the list of available fields")
+            custom_fields_system_names[field] = case_fields[labels.index(field)]["system_name"]
+
+        return custom_fields_system_names
+
 
 if __name__ == "__main__":
     tr = TestRail()
-    a = tr.create_case(TestCase("Test", "Test description"))
+    a = tr.construct_custom_fields(["Preconditions", "Automation Type"])
+    # a = tr.create_case(TestCase("Test", "Test description"))
     b = 1
